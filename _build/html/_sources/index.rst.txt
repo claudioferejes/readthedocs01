29a4e18402946dd926f56656622f15316d369cd4
Claudio Fernandes <claudio.fe.rejes@gmail.com>
2023-08-31 23:31:03 -0300

    Projeto ReadtheDocs- v1.0.0

---
+++ _build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. Road-MAP-DevOPS documentation master file, created by
+   sphinx-quickstart on Thu Aug 31 22:59:52 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Road-MAP-DevOPS's documentation!
+===========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ conf.py
@@ -0,0 +1,32 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Road-MAP-DevOPS'
+copyright = '2023, CFRJ'
+author = 'CFRJ'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',    # Geração automática de documentação a partir do código-fonte
+    'sphinx.ext.napoleon',   # Suporte para docstrings estilo Google
+    'sphinx.ext.viewcode',   # Links para o código-fonte
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'pt_BR'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
